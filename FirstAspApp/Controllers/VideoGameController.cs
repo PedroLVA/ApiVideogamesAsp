dc265ced71a87a500c88b6ceb02edc2e07fd600e
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-28 16:43:43 -0300

    feat: add VideoGameController

---
+++ FirstAspApp/Controllers/VideoGameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+    }
+}
