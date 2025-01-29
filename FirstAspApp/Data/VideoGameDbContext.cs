aaf7f482c03df3e6e3cecbe7fa119209c5193a08
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-29 15:29:06 -0300

    feat: add migration

--- FirstAspApp/Data/VideoGameDbContext.cs
+++ FirstAspApp/Data/VideoGameDbContext.cs
@@ -4,6 +4,6 @@ namespace FirstAspApp.Data
 {
     public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
     {
-
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
     }
 }
