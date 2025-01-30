a63c08134a8eb17356b67ca55a5b97fcd20031ff
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-30 14:51:47 -0300

    feat: add Genre Model

---
+++ FirstAspApp/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstAspApp.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public List<VideoGame>? ListOfVideogames { get; set; }
+
+
+
+    }
+}
