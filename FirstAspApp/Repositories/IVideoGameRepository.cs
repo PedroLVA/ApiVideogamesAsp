369b67fa8be38af41835fbcb164b34476d37ac03
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-31 15:55:37 -0300

    feat: IVideoGameRepository

---
+++ FirstAspApp/Repositories/IVideoGameRepository.cs
@@ -0,0 +1,19 @@
+﻿using FirstAspApp.Models;
+
+namespace FirstAspApp.Repositories
+{
+    public interface IVideoGameRepository
+    {
+        List<VideoGame> GetAllVideoGames();
+
+        VideoGame GetVideoGameById(int id);
+
+        void AddVideoGame(VideoGame videoGame);
+
+        void UpdateVideoGame(VideoGame videoGame);
+
+        void DeleteVideoGame(int id);
+
+
+    }
+}
