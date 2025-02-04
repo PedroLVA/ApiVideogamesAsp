44c4e8dcee1dd0e83e7fd41af2cba5752fae62f2
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-04 13:47:37 -0300

    feat: add IPublisherRepository

---
+++ FirstAspApp/Interfaces/IPublisherRepository.cs
@@ -0,0 +1,13 @@
+﻿using FirstAspApp.Models;
+
+namespace FirstAspApp.Interfaces
+{
+    public interface IPublisherRepository
+    {
+        Task<List<Publisher>> GetAllPublishers();
+        Task<Publisher?> GetPublisherById(int id);
+        Task<Publisher> AddPublisher(Publisher publisher);
+        Task UpdatePublisher(Publisher publisher);
+        Task DeletePublisher(int id);
+    }
+}
