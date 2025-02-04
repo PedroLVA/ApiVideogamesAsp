050fd03194ef9c1149f0d0b4c5bc9dacdf4be029
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-04 13:54:28 -0300

    feat: add empty PublisherRepository

--- FirstAspApp/Program.cs
+++ FirstAspApp/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddScoped<IVideoGameRepository, VideoGameRepository>();
 
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
 
+builder.Services.AddScoped<IPublisherRepository, PublisherRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ FirstAspApp/Repositories/PublisherRepository.cs
@@ -0,0 +1,40 @@
+﻿using FirstAspApp.Data;
+using FirstAspApp.Interfaces;
+using FirstAspApp.Models;
+
+namespace FirstAspApp.Repositories
+{
+    public class PublisherRepository : IPublisherRepository
+    {
+        private readonly VideoGameDbContext _context;
+
+        public PublisherRepository(VideoGameDbContext context) {
+            _context = context;
+        }
+
+        public Task<Publisher> AddPublisher(Publisher publisher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePublisher(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Publisher>> GetAllPublishers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Publisher?> GetPublisherById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePublisher(Publisher publisher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
