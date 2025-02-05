6385866cc65183bb5c36d38b4aa4b6123de24992
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-05 09:15:29 -0300

    feat: add IDeveloperRepository

---
+++ FirstAspApp/Interfaces/IDeveloperRepository.cs
@@ -0,0 +1,19 @@
+﻿using FirstAspApp.Models;
+
+namespace FirstAspApp.Interfaces
+{
+    public interface IDeveloperRepository
+    {
+        Task<List<Developer>> GetAllDevelopers();
+        
+        Task<Developer?> GetDeveloperById(int id);
+
+        Task<Developer> AddDeveloper(Developer developer);
+
+        Task UpdateDeveloper(Developer developer);
+
+        Task DeleteDeveloper(int id);
+
+        Task<Developer> GetDeveloperByName(string name);
+    }
+}
