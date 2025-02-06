c9e0e13f491ad5e5245c26b1c0f72e595b703402
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-06 10:43:35 -0300

    feat: add UserDTO

---
+++ FirstAspApp/DTOs/UserDTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstAspApp.DTOs.UserDTOs
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
