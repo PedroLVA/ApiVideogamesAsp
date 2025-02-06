bf21651861f568d74dae18a63a0afb63a1e54422
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-06 10:44:00 -0300

    feat: add AuthController

---
+++ FirstAspApp/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using FirstAspApp.DTOs.UserDTOs;
+using FirstAspApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDTO userDTO)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, userDTO.Password);
+
+            user.UserName = userDTO.UserName;
+            user.Email = userDTO.Email;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+    }
+}
