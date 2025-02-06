961d16affa8128bb3c1a468da910faa611c9ee9d
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-06 11:56:40 -0300

    feat: add user migration

--- FirstAspApp/Migrations/20250206144153_UserEntity.Designer.cs
+++ FirstAspApp/Migrations/20250206144153_UserEntity.Designer.cs
@@ -4,6 +4,7 @@ using FirstAspApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FirstAspApp.Migrations
 {
     [DbContext(typeof(VideoGameDbContext))]
-    partial class VideoGameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250206144153_UserEntity")]
+    partial class UserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
