4ce4dd0364fd42aeaa7a1941f1aa4011ed75da90
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-11 15:22:41 -0300

    feat: add add and delete review methods

--- FirstAspApp/Repositories/ReviewRepository.cs
+++ FirstAspApp/Repositories/ReviewRepository.cs
@@ -1,31 +1,49 @@
-﻿using FirstAspApp.Interfaces;
+﻿using FirstAspApp.Data;
+using FirstAspApp.Interfaces;
 using FirstAspApp.Models;
 
 namespace FirstAspApp.Repositories
 {
     public class ReviewRepository : IReviewRepository
     {
-        public Task<Review> AddReview(Review review)
+        private readonly VideoGameDbContext _context;
+
+        public ReviewRepository(VideoGameDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Review> AddReview(Review review)
+        {
+            _context.Reviews.Add(review);
+
+            await _context.SaveChangesAsync();
+
+            return review;
+        }
+
+        public async Task DeleteReview(int id)
+        {
+            var foundReview = await _context.Reviews.FindAsync(id);
+
+            _context.Reviews.Remove(foundReview);
+            await _context.SaveChangesAsync();
+
+            return;
+
+        }
+
+        public async Task<List<Review>> GetAllReviews()
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteReview(int id)
+        public async Task<Review> GetReviewById(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<Review>> GetAllReviews()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Review> GetReviewById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateReview(Review review)
+        public async Task UpdateReview(Review review)
         {
             throw new NotImplementedException();
         }
