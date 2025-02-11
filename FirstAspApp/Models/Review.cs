1b6f8c28ced664e5a00bac33593c702a1820a738
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-11 11:56:42 -0300

    feat: add Review model

---
+++ FirstAspApp/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstAspApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }  
+        public int GameId { get; set; }
+        public int UserId { get; set; }
+        public string? Comment { get; set; }
+        public int Rating { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
