04f1e6e3296bda6d9878fc387a37d2abecccee7e
Pedro Athayde <pedro_athayde@outlook.com>
2025-02-17 14:07:11 -0300

    feat: add platform entity

---
+++ FirstAspApp/Models/Platform.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstAspApp.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }  // (e.g., PC, PlayStation, Xbox, Switch)
+        public required string Company { get; set; }
+
+        public List<VideoGame>? VideoGames { get; set; }
+
+    }
+}
